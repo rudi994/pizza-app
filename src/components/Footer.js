cebc13190ba9a49fbd001b5c301744f5e7a9c785
rudi994 <rutujapatankar1306@gmail.com>
2024-10-07 17:57:05 +0530

    Initial commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Menu } from 'semantic-ui-react';
 const Footer = () => (
   <Menu fluid widths={1} fixed='bottom' id='footer'>
     <Menu.Item>
-      <p>© 2018 Slices Pizza<span role='img' aria-label='pizza'> 🍕🍕🍕 Built by <a href='http://pamela.io' target="_blank" rel="noopener noreferrer">pamela.io</a></span></p>
+      <p>©Slices by Rutuja<span role='img' aria-label='pizza'> </span></p>
     </Menu.Item>
   </Menu>
 );
